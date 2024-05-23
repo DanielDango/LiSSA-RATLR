e71b54b90d1630527bfbd15039a37e0d4aa90305
Dominik Fuchß <dominik.fuchss@kit.edu>
2024-05-23 15:24:05 +0200

    Fixed key for caching

--- src/main/java/edu/kit/kastel/sdq/lissa/ratlr/embeddingcreator/CachedEmbeddingCreator.java
+++ src/main/java/edu/kit/kastel/sdq/lissa/ratlr/embeddingcreator/CachedEmbeddingCreator.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 abstract class CachedEmbeddingCreator extends EmbeddingCreator {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -27,7 +28,7 @@ abstract class CachedEmbeddingCreator extends EmbeddingCreator {
     public final List<float[]> calculateEmbeddings(List<Element> elements) {
         List<float[]> embeddings = new ArrayList<>();
         for (Element element : elements) {
-            String key = element.getIdentifier();
+            String key = UUID.nameUUIDFromBytes(element.getContent().getBytes()).toString();
             float[] cachedEmbedding = cache.get(key, float[].class);
             if (cachedEmbedding != null) {
                 embeddings.add(cachedEmbedding);
